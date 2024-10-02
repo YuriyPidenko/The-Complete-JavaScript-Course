4d9a4e99d57931edf692bedded2612100574323b
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-10-02 19:17:15 +0300

    tasks

---
+++ Полный Курс По JavaScript/TASKS/12-create-object/script.js
@@ -0,0 +1,18 @@
+/** ЗАДАЧА - Создание объекта
+ * 
+ * 1.Создайте объект с тремя свойствами:
+ * -name
+ * -surname
+ * -favoriteNumber
+ * 
+ * 2.Выведите в консоль строку 
+ *  "My name is <name> <surnam> and my favorite is <favoriteNumber>"
+ */
+
+const nameObject = {
+    name: 'Bob',
+    surname: 'Bagdab',
+    favoriteNumber: 23
+}
+
+console.log(`My name is ${nameObject.name} ${nameObject.surname} and my favotite is ${nameObject.favoriteNumber}`);
